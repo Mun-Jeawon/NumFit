3670c1748502ada18f443a9784a28bf7c6c51d47
Mun Jeawon <mjo3567@gmail.com>
2023-12-01 20:09:16 +0900

    시도횟수 추가, 코드변경,연습장 추가

--- 연습장.py
+++ 연습장.py
@@ -1,3 +1,62 @@
+#from tkinter import*
+#import random
+#
+#answer=random.randint(1,100)
+#
+#def guessing():
+#    guess=int(guessField.get())
+#    
+#    if guess>answer:
+#        msg="높음!!"
+#    elif guess<answer:
+#        msg="낮음!!"
+#    else:
+#        msg="정답!!"
+#    
+#    resultLabel["text"] = msg
+#    guessField.delete(0, 5)
+#
+#def reset(): 
+#	global answer
+#	answer = random.randint(1,100)
+#	resultLabel["text"] = "다시 한번 하세요!"
+#
+#window = Tk()
+#window.configure(bg="white")
+#window.title("숫자를 맞춰보세요!")
+#
+#
+#canvas = Canvas(window, width=650, height=100, bg = '#afeeee')
+#canvas.create_text(300, 50, fill="darkblue", font="Times 30 italic bold",
+#                        text="분반:3 학번:20201267 이름:문재원")
+#canvas.pack()
+#
+#
+#counter = 0
+#
+#def clicked():
+#    global counter
+#    counter += 1
+#    label['text'] = '시도 횟수: ' + str(counter)
+#        
+#label = Label(window, text="아직 눌려지지 않음")
+#label.pack()
+#guessField = Entry(window)
+#guessField.pack(side="left")
+#tryButton = Button(window, text="시도", fg="green", bg="white",
+#command=clicked and guessing)
+#tryButton.pack(side="left")
+#
+#
+#resetButton = Button(window, text="초기화", fg="red", bg="white",
+#command=reset)
+#resetButton.pack(side="left")
+#resultLabel = Label(window, text="1부터 100사이의 숫자를 입력하시오.",
+#bg="white")
+#resultLabel.pack(side="left")
+#
+#window.mainloop()
+
 from tkinter import *
 import random
 
@@ -31,10 +90,12 @@ window = Tk()
 window.configure(bg="white")
 window.title("숫자를 맞춰보세요!")
 
+
 canvas = Canvas(window, width=650, height=100, bg = '#afeeee')
 canvas.create_text(300, 50, fill="darkblue", font="Times 30 italic bold",
                         text="분반:3 학번:20201267 이름:문재원")
 canvas.pack()
+
 
 counter = 0
 
@@ -59,5 +120,7 @@ resetButton.pack(side="left")
 resultLabel = Label(window, text="1부터 100사이의 숫자를 입력하시오.",
 bg="white")
 resultLabel.pack(side="left")
+
+window.mainloop()
 
 window.mainloop()
