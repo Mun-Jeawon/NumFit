e5ac25af99884c0eecbc8ae7238ba1c084f24015
Mun Jeawon <mjo3567@gmail.com>
2023-12-02 19:38:31 +0900

    화살표 추가

--- 연습장.py
+++ 연습장.py
@@ -8,10 +8,13 @@ def guessing():
     
     if guess > answer:
         msg = "높음!!"
+        update_arrow("C:/numfit/row_arrow.png")
     elif guess < answer:
         msg = "낮음!!"
+        update_arrow("C:/numfit/high_arrow.png")
     else:
         msg = "정답!!"
+        update_arrow("C:/numfit/snowman.png")
     
     resultLabel["text"] = msg
     guessField.delete(0, END)  
@@ -20,6 +23,15 @@ def reset():
     global answer
     answer = random.randint(1, 100)
     resultLabel["text"] = "다시 한번 하세요!"
+    clear_arrow()
+
+def update_arrow(image_path):
+    img = PhotoImage(file=image_path)
+    arrow_canvas.img = img  
+    arrow_canvas.create_image(20, 20, anchor=NW, image=img)
+
+def clear_arrow():
+    arrow_canvas.delete("all")
 
 def clicked():
     clicked.counter += 1
@@ -31,10 +43,10 @@ window = Tk()
 window.configure(bg="white")
 window.title("숫자를 맞춰보세요!")
 
-canvas = Canvas(window, width=650, height=100, bg='#afeeee')
-canvas.create_text(300, 50, fill="darkblue", font="Times 30 italic bold",
+info_canvas = Canvas(window, width=650, height=100, bg='#afeeee')
+info_canvas.create_text(300, 50, fill="darkblue", font="Times 30 italic bold",
                    text="분반:3 학번:20201267 이름:문재원")
-canvas.pack()
+info_canvas.pack()
 
 label = Label(window, text="아직 눌려지지 않음")
 label.pack()
@@ -52,11 +64,8 @@ resultLabel = Label(window, text="1부터 100사이의 숫자를 입력하시오
                     bg="white")
 resultLabel.pack(side="left")
 
-canvas = Canvas(window, width=70, height=70)
-canvas.pack()
-
-img = PhotoImage(file="C:/numfit/snowman.png")  # 이미지 경로 수정
-canvas.create_image(20, 20, anchor=NW, image=img)
+arrow_canvas = Canvas(window, width=70, height=70)
+arrow_canvas.pack(side="left")
 
 clicked.counter = 0
 
