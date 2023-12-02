586a1408e5896cffd94428d9e233cbbcfe9f7bb9
Mun Jeawon <mjo3567@gmail.com>
2023-12-02 15:05:00 +0900

    눈사람 추가

--- NumFit.py
+++ NumFit.py
@@ -14,7 +14,7 @@ def guessing():
         msg = "정답!!"
     
     resultLabel["text"] = msg
-    guessField.delete(0, END)  # Entry 내용을 지우는 방법 수정
+    guessField.delete(0, 5)  
 
 def reset(): 
     global answer
@@ -31,33 +31,34 @@ window = Tk()
 window.configure(bg="white")
 window.title("숫자를 맞춰보세요!")
 
-canvas = Canvas(window, width=650, height=100, bg = '#afeeee')
+canvas = Canvas(window, width=650, height=100, bg='#afeeee')
 canvas.create_text(300, 50, fill="darkblue", font="Times 30 italic bold",
-                        text="분반:3 학번:20201267 이름:문재원")
+                   text="분반:3 학번:20201267 이름:문재원")
 canvas.pack()
 
-counter = 0
-
-def clicked():
-    global counter
-    counter += 1
-    label['text'] = '시도 횟수: ' + str(counter)
-        
 label = Label(window, text="아직 눌려지지 않음")
 label.pack()
 guessField = Entry(window)
 guessField.pack(side="left")
 
 tryButton = Button(window, text="시도", fg="green", bg="white",
-                   command=lambda: (clicked(), guessing()))  # 람다 함수로 변경
+                   command=guessing)  
 tryButton.pack(side="left")
 
-
 resetButton = Button(window, text="초기화", fg="red", bg="white",
-command=reset)
+                     command=reset)
 resetButton.pack(side="left")
 resultLabel = Label(window, text="1부터 100사이의 숫자를 입력하시오.",
-bg="white")
+                    bg="white")
 resultLabel.pack(side="left")
 
+canvas = Canvas(window, width=70, height=70)
+canvas.pack()
+
+img = PhotoImage(file="C:/numfit/snowman.png")  # 이미지 경로 수정
+canvas.create_image(20, 20, anchor=NW, image=img)
+
+clicked.counter = 0
+
 window.mainloop()
+
